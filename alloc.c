0258500d9a7a4faa1d7ef02df8ec8792958055d1
odysseus <rb9362123@gmail.com>
2026-03-11 14:09:15 -0400

    started on malloc

--- alloc.c
+++ alloc.c
@@ -3,12 +3,22 @@
 
 extern heap *memspace;
 
+void *alloc(int32 bytes)
+{
+    //convert bytes to number of words
+    word words;
+
+    words = (!(bytes % 4)) ? bytes / 4 : (bytes/4) + 1;
+
+    //find hole
+    
+}
+
 
 int main(int argc, char *argv[])
 {
-    int8 *p;
+    
 
-    p = memspace;
-    *p = 'X'; 
+
     return 0;
 }
